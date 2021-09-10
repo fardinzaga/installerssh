8eac428c4208c026864cb187f448d9f22f648823
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-10 12:08:56 +0700

    Update websocket.sh

--- websocket/websocket.sh
+++ websocket/websocket.sh
@@ -20,7 +20,7 @@ wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.co
 #System SSL/TLS Websocket-SSH Python
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-stunnel.service
 ##System Websocket-OpenVPN Python
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-ovpn.service
 
 #Install Script Websocket-SSH Python
 cd
