92dfd5510e6b85df3ee217c0e072fa7e57c877e0
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-15 15:32:31 +0700

    Update websocket.sh

--- websocket/websocket.sh
+++ websocket/websocket.sh
@@ -24,12 +24,14 @@ cd /usr/local/bin/
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-openssh && chmod +x /home/admin/ws-openssh
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-dropbear && chmod +x /home/admin/bin/ws-dropbear
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-stunnel && chmod +x /home/admin/ws-stunnel
+wget -0 /usr/local/bin/ws-fauzanvpn https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws-fauzanvpn && chmod +x /home/admin/ws-fauzanvpn
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/installerssh/master/websocket/ws.ovpn && chmod +x /home/admin/ws-ovpn
 
 #izin permision
 chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
+chmod +x /usr/local/bin/ws-fauzanvpn
 #chmod +x /usr/local/bin/ws-ovpn
 
 #restart service
@@ -49,6 +51,11 @@ systemctl restart ws-dropbear.service
 systemctl enable ws-stunnel.service
 systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
+
+#Enable & Start & Restart ws-fauzanvpn service
+systemctl enable ws-fauzanvpn.service
+systemctl start ws-fauzanvpn.service
+systemctl restart ws-fauzanvpn.service
 
 #Enable & Start ws-ovpn service
 #systemctl enable ws-ovpn.service
