fa304a4634a7dea096e3768a465cb67842d2b59b
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-15 11:13:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22, 500"  | tee -a log-install.txt
 echo "   - SSH-WS CDN OpenSSH       : 2086"  | tee -a log-install.txt
-echo "   - SSH-WS CDN Dropbear      : 2082"  | tee -a log-install.txt
+echo "   - SSH-WS CDN Dropbear      : 2082, 2083"  | tee -a log-install.txt
 echo "   - SSH-WS CDN SSL/TLS       : 443"  | tee -a log-install.txt
 echo "   - Websocket                : 80"  | tee -a log-install.txt
 echo "   - OpenVPN-WS               : 2092"  | tee -a log-install.txt
