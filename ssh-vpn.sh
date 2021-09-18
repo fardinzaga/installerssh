9c5164b5e93c2e775ed218726b53683722dfc779
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-19 05:35:46 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -117,6 +117,7 @@ sed -i '/Port 22/a Port 88' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 5000' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 400' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 523' /etc/ssh/sshd_config
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
