4d2f8a8b16416a9eba22de27205e0b57d3daf807
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-19 13:10:56 +0700

    Update hazrian-ws.py

--- websocket/hazrian-ws.py
+++ websocket/hazrian-ws.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:200'
-RESPONSE = 'HTTP/1.1 101 <b><u><font color="cyan">Setup By Geo t.me/sampiiiiu</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="green">Switching Protocol</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
