e8cb0a58f1412a9dc1539e44b37cb1f25e7771b8
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-23 10:06:24 +0700

    Update slhost.sh

--- menu/slhost.sh
+++ menu/slhost.sh
@@ -1,4 +1,4 @@
 #!/bin/bashf
 read -rp "Domain/Host: " -e host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-echo "Domain=$host" >> /etc/v2ray/domain
+echo "domain=$host" >> /etc/v2ray/domain
