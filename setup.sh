c71f108c1ef53fe34ad72504918fc6d539747938
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-04 20:54:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/ssh-vpn.sh
 #install ssh ws
 wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/websket/install-ws.sh && chmod +x install-ws.sh && screen -S install-ws.sh ./install-ws.sh
 #install v2ray
-https://raw.githubusercontent.com/fardinzaga/installerssh/master/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/install-ws.sh
