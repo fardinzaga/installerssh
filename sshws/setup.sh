2fe4c6ae269666f9016c5b995ef6646696dfb6f3
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-19 17:51:38 +0700

    Update setup.sh

--- sshws/setup.sh
+++ sshws/setup.sh
@@ -27,7 +27,7 @@ echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
 read -p "Hostname / Domain: " host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
-
+https://raw.githubusercontent.com/fardinzaga/installerssh/master/sshws/cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/sshws/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
 #install ssh ws
