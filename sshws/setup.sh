8ec53452ef90e870594346b4afa97ed56ed3a181
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-25 12:33:44 +0700

    Update setup.sh

--- sshws/setup.sh
+++ sshws/setup.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 if [ "${EUID}" -ne 0 ]; then
-  echo "You need to run this script as root"
-  exit 1
+		echo "You need to run this script as root"
+		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-  echo "OpenVZ is not supported"
-  exit 1
+		echo "OpenVZ is not supported"
+		exit 1
 fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
+echo "Authentikasi pada server"
+IZIN=$( curl icanhazip.com | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permintaan Diterima...${NC}"
+else
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
 exit 0
 fi
 mkdir /var/lib/premium-script;
 mkdir /etc/v2ray;
-echo "IP=" >> /var/lib/premium-script/ipvps.conf
+echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
-wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/sshws/cf.sh && chmod +x cf.sh && ./cf.sh
+
 #install ssh ovpn
-wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/sshws/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
 #install ssh ws
 wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/websket/install-ws.sh && chmod +x install-ws.sh && screen -S install-ws.sh ./install-ws.sh
+#install v2ray
+wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/install-ws.sh
+rm -f /root/ins-vt.sh
 history -c
 echo "1.1" > /home/ver
 clear
@@ -49,6 +60,11 @@ echo "   - Dropbear                 : 143, 109"  | tee -a log-install.txt
 echo "   - Squid Proxy              : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                   : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                    : 81"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS          : 8443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS     : 80"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS          : 2083"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS     : 8880"  | tee -a log-install.txt
+echo "   - Trojan                   : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
@@ -57,7 +73,7 @@ echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 08.00 GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On 08.00 - 00.00 GMT +7" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "------------------------------------------Mod by Fauzanvpn------------------------------------------" | tee -a log-install.txt
