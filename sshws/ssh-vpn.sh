c27e1cd09a7da930603e94272d575d2a3facf112
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-25 16:05:45 +0700

    Update ssh-vpn.sh

--- sshws/ssh-vpn.sh
+++ sshws/ssh-vpn.sh
@@ -311,8 +311,7 @@ chmod +x clear-log
 echo "================  Auto deleted Account Expired ======================"
 wget -O /usr/local/bin/userdelexpired "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/userdelexpired" && chmod +x /usr/local/bin/userdelexpired
 
-echo "0 0 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 0 * * * root xp" >> /etc/crontab
+echo "0 0 * * * root /usr/local/bin/user-expire" > /etc/cron.d/user-expire
 
 # remove unnecessary files
 apt -y autoclean
