5668a8c2277b5eb42502e282ecb7ead01a0caf5c
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-21 12:15:46 +0700

    Update and rename autokick.sh to autokill.sh

--- menu/autokill.sh
+++ menu/autokill.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
-# Script Mod By Fauzanvpn
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[ON]${Font_color_suffix}"
+Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
+cek=$(grep -c -E "^# Autokill" /etc/cron.d/tendang)
+if [[ "$cek" = "1" ]]; then
+sts="${Info}"
+else
+sts="${Error}"
+fi
 clear
 echo -e ""
 echo -e "======================================"
 echo -e ""
-echo -e "     AutoKick Mod by Fauzanvpn      "
+echo -e "     Status Autokill $sts        "
 echo -e ""
-echo -e "     [1]  AutoKick After 5 Minutes"
-echo -e "     [2]  AutoKick After 15 Minutes"
-echo -e "     [3]  AutoKick After 30 Minutes"
-echo -e "     [4]  Turn Off AutoKick/MultiLogin"
+echo -e "     [1]  AutoKill After 5 Minutes"
+echo -e "     [2]  AutoKill After 10 Minutes"
+echo -e "     [3]  AutoKill After 15 Minutes"
+echo -e "     [4]  Turn Off AutoKill/MultiLogin"
 echo -e "     [x]  Exit"
 echo -e "======================================"                                                                                                          
 echo -e ""
-read -p "     Select From Options [1-4 or x] :  " AutoKick
+read -p "     Select From Options [1-4 or x] :  " AutoKill
+read -p "     Multilogin Maximum Number Of Allowed: " max
 echo -e ""
-case $AutoKick in
+case $AutoKill in
                 1)
                 echo -e ""
                 sleep 1
                 clear
-                rm -f /etc/cron.d/tendang
-                echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >>/etc/cron.d/tendang
+                echo "*/5 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
-                echo -e "      AutoKick Mod by Fauzanvpn    "
                 echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKick Every     : 5 Minutes"      
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 5 Minutes"      
                 echo -e ""
                 echo -e "======================================"                                                                                                                                 
                 exit                                                                  
@@ -36,15 +50,14 @@ case $AutoKick in
                 echo -e ""
                 sleep 1
                 clear
-                rm -f /etc/cron.d/tendang
-                echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >>/etc/cron.d/tendang
+                echo "*/10 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      AutoKick Mod by Fauzanvpn     "
-                echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKick Every     : 15 Minutes"
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 10 Minutes"
                 echo -e ""
                 echo -e "======================================"
                 exit
@@ -53,28 +66,25 @@ case $AutoKick in
                 echo -e ""
                 sleep 1
                 clear
-                rm -f /etc/cron.d/tendang
-                echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >>/etc/cron.d/tendang
+                echo "*/15 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      AutoKick Mod by Fauzanvpn     "
-                echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKick Every     : 30 Minutes"
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 15 Minutes"
                 echo -e ""
                 echo -e "======================================"
                 exit
                 ;;
                 4)
                 clear
-                rm -f /etc/cron.d/tendang
+                echo > /etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      AutoKick Mod by Fauzanvpn     "
-                echo -e ""
-                echo -e "      AutoKick MultiLogin Turned Off  "
+                echo -e "      AutoKill MultiLogin Turned Off  "
                 echo -e ""
                 echo -e "======================================"
                 exit
