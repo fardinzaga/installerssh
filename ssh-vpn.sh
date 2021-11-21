7e5c32a5293c789395ecc53f199d96a39f50d4ab
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-21 13:48:10 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -270,7 +270,7 @@ cd
 apt install -y libxml-parser-perl
 
 # banner /etc/bnr
-wget -O /etc/bnr "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/banner/bnr"
+wget -O /etc/bnr "https://raw.githubusercontent.com/fardinzaga/installerssh/master/banner/bnr"
 wget -O /etc/banner "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/banner/banner"
 sed -i 's@#Banner@Banner /etc/banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/bnr"@g' /etc/default/dropbear
